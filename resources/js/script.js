ac33dacf18388d15adbb3fa276213e8ce474a27a
hellburner90 <hellburner90@github.com>
2020-11-04 09:05:23 +0530

    added new files

---
+++ resources/js/script.js
@@ -0,0 +1,60 @@
+/* global $ */
+
+
+
+/* eslint-env jquery */
+$(document).ready(function() {
+     /* For the sticky navigation */                                  
+  $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+});
+   
+    
+    /* For the scroll on buttons */
+    
+    $('.js--scroll-to-plans').click(function () {
+        $('httml, body').animate({scrollTop: $(js--section-plans).offset().top}, 1000);
+    });  
+    $('.js--scroll-to-start').click(function () {
+        $('httml, body').animate({scrollTop: $(js--section-features).offset().top}, 1000);
+    });
+    
+    
+});
+
+   /* navigation scroll */
+$(function() {
+
+      $('a[href*="#"]:not([href="#"])').click(function() {
+
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+          var target = $(this.hash);
+
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+          if (target.length) {
+
+            $('html,body').animate({
+
+              scrollTop: target.offset().top
+
+            }, 1000);
+
+            return false;
+
+          }
+
+        }
+
+      });
+    
+
+    });
+
